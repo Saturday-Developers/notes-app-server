24b2ee587d06cf8eb6590d4b153472f3e544d197
Vladislav Stepanov <st.vladislav.97@gmail.com>
2020-04-29 08:49:06 +0300

    Change local default database

--- app/app/settings/local.py
+++ app/app/settings/local.py
@@ -87,8 +87,10 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresql',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
