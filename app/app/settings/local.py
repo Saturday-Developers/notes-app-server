4b573cdc0c377a5a319f4f49e5e78f56c75b1403
Vladislav Stepanov <st.vladislav.97@gmail.com>
2020-04-29 09:49:39 +0300

    Test github CI

--- app/app/settings/local.py
+++ app/app/settings/local.py
@@ -87,10 +87,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgresql',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'postgresql',
+        # 'HOST' : '127.0.0.1',
+        # 'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
